ebcfd642bd6969258aa806ae87e9c4602ae29fe9
Stefan Eilemann <SEilemann@esri.com>
2019-08-12 11:54:02 +0200

    aBee: allow json url as webScene parameter

--- aBee/app/main.js
+++ aBee/app/main.js
@@ -1,14 +1,14 @@
 require([
   "dojo/has",
   "esri/config",
+  "esri/request",
   "esri/WebScene",
   "esri/layers/Layer",
   "esri/views/SceneView",
   "app/syncUtil"
-], function (has, config, WebScene, Layer, SceneView, syncUtil) {
-
+], function(has, config, esriRequest, WebScene, Layer, SceneView, syncUtil) {
   var params = {};
-  var parts = window.parent.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+  var parts = window.parent.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
     params[key] = value;
   });
 
@@ -19,28 +19,31 @@ require([
     config.portalUrl = portal;
   }
 
-  var url = params["url"]
-  var webscene;
+  var view = new SceneView({ container: "view", map: webscene });
+  var url = params["url"];
+
   if (url) {
-    webscene = new WebScene({basemap: "topo", ground: "world-elevation"});
-    Layer.fromArcGISServerUrl({url: url}).then(function(layer){
-      webscene.layers.add(layer);
-      layer.when(function(){
-        return layer.queryExtent();
-      })
-      .then(function(response){
-        view.goTo(response.extent);
-      });
+    view.map = new WebScene({ basemap: "topo", ground: "world-elevation" });
+    Layer.fromArcGISServerUrl({ url: url }).then(function(layer) {
+      view.map.layers.add(layer);
+      layer
+        .when(function() {
+          return layer.queryExtent();
+        })
+        .then(function(response) {
+          view.goTo(response.extent);
+        });
     });
   } else {
-    webscene = params["webscene"] || "46c47340708f446ba7f112f139e8ae5e";
-    webscene = new WebScene({ portalItem: { id: webscene }});
+    var webscene = params["webscene"] || "46c47340708f446ba7f112f139e8ae5e";
+    if (webscene.startsWith("http")) {
+      esriRequest(webscene).then(function(json) {
+        view.map = WebScene.fromJSON(json.data);
+      });
+    } else {
+      view.map = new WebScene({ portalItem: { id: webscene } });
+    }
   }
-
-  var view = new SceneView({
-    container: "view",
-    map: webscene,
-  });
 
   // Clear the top-left corner to make place for the title
   view.ui.empty("top-left");
@@ -53,14 +56,12 @@ require([
     // The view must be ready (or resolved) before you can
     // access the properties of the WebScene
     view.when(function() {
-
       // The slides are a collection inside the presentation
       // property of the WebScene
       var slides = webscene.presentation.slides;
 
       // Loop through each slide in the collection
       slides.forEach(function(slide) {
-
         // Create a new <div> element for each slide and place the title of
         // the slide in the element.
         var slideElement = document.createElement("div");
@@ -85,11 +86,11 @@ require([
 
   function updateStats() {
     setTimeout(updateStats, 1000);
-    if (view.getStats){
+    if (view.getStats) {
       var stats = view.getStats();
       var textContent = "";
       var keys = Object.keys(stats);
-      for (var i = 0 ; i < keys.length; ++i) {
+      for (var i = 0; i < keys.length; ++i) {
         textContent += "<br/>" + keys[i] + ": " + stats[keys[i]];
       }
 
@@ -97,13 +98,21 @@ require([
     } else {
       var rc = view.resourceController;
       var mc = rc.memoryController || rc._memoryController;
-      if  (mc && mc._cacheStorage) {
+      if (mc && mc._cacheStorage) {
         document.getElementById("stats").innerHTML =
-          "Memory: " + (mc._memoryUsed * mc._maxMemory).toFixed() + " of " + mc._maxMemory.toFixed() + "MB<br>" +
-          "Cache: " + (mc._cacheStorage._size / 1048576).toFixed() + " of " + (mc._cacheStorage._maxSize / 1048576).toFixed() + "MB<br>";
+          "Memory: " +
+          (mc._memoryUsed * mc._maxMemory).toFixed() +
+          " of " +
+          mc._maxMemory.toFixed() +
+          "MB<br>" +
+          "Cache: " +
+          (mc._cacheStorage._size / 1048576).toFixed() +
+          " of " +
+          (mc._cacheStorage._maxSize / 1048576).toFixed() +
+          "MB<br>";
       }
     }
-  };
+  }
 
   setTimeout(updateStats, 1000);
 });
